bee1d86925f81d4b12de9c66cec8f79deb30a307
Justin Caratzas <bigjust@lambdaphil.es>
2016-10-14 22:09:43 -0400

    reveal question 30 seconds after answer

--- helga_jeopardy.py
+++ helga_jeopardy.py
@@ -1,13 +1,28 @@
 import requests
 
+from twisted.internet import reactor
+
 from helga.plugins import command
+
+
+api_endpoint = 'http://www.trivialbuzz.com/api/v1/'
+
+def reveal_answer(client, channel, args):
+    client.msg(channel, 'the correct answer is: {}'.format(args))
 
 @command('j', help='jeopardy!')
 def jeopardy(client, channel, nick, message, cmd, args):
+
     try:
-        tb_resp = requests.get('http://www.trivialbuzz.com/api/v1/questions/random.json')
-        question_text = tb_resp.json()['question']['body']
+
+        tb_resp = requests.get('{}questions/random.json'.format(api_endpoint)).json()['question']
+        question_text = tb_resp['body']
+        answer = tb_resp['response']
+        category = tb_resp['category']['name']
+
+        reactor.callLater(30, reveal_answer, client, channel, answer)
+
     except requests.exceptions.RequestException:
         return 'problem, check logs'
 
-    return question_text
+    return '[{}] {}'.format(category, question_text)
